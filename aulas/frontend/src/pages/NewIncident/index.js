d57ab2c6e5e226a8386142e2cf04ac0118f9fb77
Wallace Ferreira <mr.wallaceferreira@gmail.com>
2020-03-28 23:11:10 -0300

    Exibição de cards cadastrados

---
+++ aulas/frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import { Link }  from 'react-router-dom'
+//import { FiArrowLeft }  from 'react-icons/fi'
+
+//import './styles.css';
+
+//import logoImg from '../../assets/logo.svg';    
+
+export default function NewIncident() {
+    return <h1> new Incident</h1>
+}
